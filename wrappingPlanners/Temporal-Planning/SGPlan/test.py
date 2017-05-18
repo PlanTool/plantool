7d97b53fb29b97d06a6298e31df4efb2830c265f
tianhan4 <tianhan_4@aliyun.com>
2017-05-18 13:22:30 +0800

    sgplan temporal-version wrapped.

---
+++ wrappingPlanners/Temporal-Planning/SGPlan/test.py
@@ -0,0 +1,11 @@
+import sgplan
+
+factfile = '/home/henrysky/Research/plantool/plantool/code/Deterministic/SGplan/seq-sat-sgplan6/fixit_ops.pddl'
+
+operatorfile = '/home/henrysky/Research/plantool/plantool/code/Deterministic/SGplan/seq-sat-sgplan6/domain_ops.pddl'
+
+sgplan.oldmain(['sgplan', '-f', factfile, '-o', operatorfile, '-out', 'result.soln2'])
+
+#, '-f', factfile, '-o', operatorfile
+
+print('!!')
