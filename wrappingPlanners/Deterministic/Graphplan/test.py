2068fa4a6a4e92e8b896ab735ffc0489f3c7ea12
326623 <yangqp5@mail2.sysu.edu.cn>
2017-05-24 15:48:51 +0800

    adding test file

---
+++ wrappingPlanners/Deterministic/Graphplan/test.py
@@ -0,0 +1,10 @@
+
+import graphplan
+
+operatorfile = 'fixit_ops'
+
+factfile = 'fixit_facts1'
+
+graphplan.run(['graphplan', '-f', factfile, '-o', operatorfile, '-i', '0', '-M', '256', '-t', '100', '-O', 'IL'])
+
+print('!!')
